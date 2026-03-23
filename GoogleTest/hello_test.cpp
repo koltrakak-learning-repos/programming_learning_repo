ff3118552e4134992a33a2092189ecc690ea21e0
KoolKevin <kevinkoltraka1011@gmail.com>
2026-03-23 17:06:55 +0100

    sd

--- GoogleTest/hello_test.cpp
+++ GoogleTest/hello_test.cpp
@@ -1,10 +1,15 @@
 #include <gtest/gtest.h>
 
-// Demonstrate some basic assertions.
+// GoogleTest groups the test results by test suites, so logically related
+// tests should be in the same test suite; in other words, the first argument
+// to their TEST() should be the same
+
+// The first argument is the name of the test suite, and the second argument
+// is the test’s name within the test suite.
 TEST(HelloTest, BasicAssertions)
 {
     // Expect two strings not to be equal.
     EXPECT_STRNE("hello", "world");
     // Expect equality.
-    EXPECT_EQ(7 * 6, 43);
+    EXPECT_EQ(7 * 6, 42);
 }
